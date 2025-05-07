0ce7237a679f4795e4b3a081e4f79786c3b84e8d
moznion <moznion@mail.moznion.net>
2025-05-07 20:15:36 +0900

    Initial commit
    Signed-off-by: moznion <moznion@mail.moznion.net>

---
+++ lib/rspec/sse/matchers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "matchers/version"
+
+module Rspec
+  module Sse
+    module Matchers
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rspec/sse/matchers/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Rspec
+  module Sse
+    module Matchers
+      VERSION = "0.1.0"
+    end
+  end
+end
